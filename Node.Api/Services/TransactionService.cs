cedfee137d391419a0c57ee1ae783e6c36926125
Pavlina Drosos <pavlinadrosos@gmail.com>
2018-03-08 00:10:39 +0200

    Removed SignTransaction() method from TransactionService

--- Node.Api/Services/TransactionService.cs
+++ Node.Api/Services/TransactionService.cs
@@ -12,21 +12,5 @@ namespace Node.Api.Services
         {
             this.dataService = dataService;
         }
-
-        public Transaction SignTransaction(Transaction transaction)
-        {
-            List<string> senderSignature = this.CalculateSenderSignature(transaction);
-
-            var signedTransaction = transaction;
-
-            signedTransaction.SenderSignature = senderSignature;
-
-            return signedTransaction;
-        }
-
-        private List<string> CalculateSenderSignature(Transaction transaction)
-        {
-            return new List<string>();
-        }
     }
 }
